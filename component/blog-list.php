a5e62e3a688a79dc7167f34dc971c78ac570abe4
quanweiwang <629526514@qq.com>
2019-05-18 09:53:16 +0800

    提交代码
    提交代码

--- component/blog-list.php
+++ component/blog-list.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *Used for index/archive/search/author/catgory/tag.
- *Meowdata.com 
+ *Meowdata.com
  *
  */
 ?>
@@ -15,9 +15,9 @@
 </div>
 </div>
 </div>
-<div class="entry-blog-list-right"> 
-<div class="post-content"> 
-<div class="post-header"> 
+<div class="entry-blog-list-right">
+<div class="post-content">
+<div class="post-header">
 <span class="category-meta">
  <?php $category = get_the_category();if($category[0]){ echo '<a href="'.get_category_link($category[0]->term_id ).'" title="查看《'.$category[0]->cat_name.'》下的所有文章 " rel="category tag" '. _post_target_blank().' >
 <i class="fa fa-folder-o"></i> '.$category[0]->cat_name.'</a>'; };?>
@@ -25,15 +25,6 @@
 <h2 class="entry-post-title">
 <a href="<?php echo get_permalink() ?>"><?php echo get_the_title().get_the_subtitle() ?></a>
 </h2>
-</div>
-
-
-<div class="post-intro-text">
-<?php echo _get_excerpt() ?>
-</div>
-<div class="post-btn">
-<a href="<?php echo get_permalink() ?>"  <?php echo _post_target_blank() ?>  title="查看更多：<?php echo get_the_title().get_the_subtitle(false) ?>" class="btn btn-sm btn-outline-danger">
-<i class="fa fa-folder-open-o"></i> 阅读更多</a>
 <div class="post-meta-list">
 <span class="list-post-author">
 <i class="fa fa-user-circle"></i> <?php the_author(); ?></span>
@@ -44,9 +35,18 @@
 <i class="fa fa-street-view"></i>  <?php echo getPostViews(get_the_ID()) ?>
 </span>
 <span class="list-post-comment">
-<i class="fa fa-comments-o"></i>  评论 <?php echo get_comments_number('0', '1', '%') ?> 
+<i class="fa fa-comments-o"></i>  评论 <?php echo get_comments_number('0', '1', '%') ?>
 </span>
-</div> 
+</div>
+</div>
+
+
+<div class="post-intro-text">
+<?php echo _get_excerpt() ?>
+</div>
+<div class="post-btn">
+<a href="<?php echo get_permalink() ?>"  <?php echo _post_target_blank() ?>  title="查看更多：<?php echo get_the_title().get_the_subtitle(false) ?>" class="btn btn-sm btn-outline-danger">
+<i class="fa fa-folder-open-o"></i> 阅读更多</a>
 </div>
 </div>
 </div>
