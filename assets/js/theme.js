62f8aa39883f5dae6f5c8bdc9e01725477d8402f
quanweiwang <629526514@qq.com>
2019-05-24 11:41:20 +0800

    新增 输入QQ自动获取昵称、邮箱
    新增 输入QQ自动获取昵称、邮箱

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,40 +1,39 @@
-
- 
 "use strict";
 
-$(document).ready(function() {
-new WOW().init();
+$(document).ready(function () {
+    new WOW().init();
+    inlojv_js_getqqinfo();
 
-$('body').on('click', '.comment-reply-link', function(){
-    addComment.moveForm( "comment-"+$(this).attr('data-commentid'), $(this).attr('data-commentid'), "respond", $(this).attr('data-postid') );
-    return false; // 阻止 a tag 跳转，这句千万别漏了
+    $('body').on('click', '.comment-reply-link', function () {
+        addComment.moveForm("comment-" + $(this).attr('data-commentid'), $(this).attr('data-commentid'), "respond", $(this).attr('data-postid'));
+        return false; // 阻止 a tag 跳转，这句千万别漏了
     });
 
-$('a[href="#search"]').on('click', function(event) {                    
-		$('#search').addClass('open');
-		$('#search > form > input[type="search"]').focus();
-	});            
-	$('#search, #search button.close').on('click keyup', function(event) {
-		if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-			$(this).removeClass('open');
-		}
-	});   
+    $('a[href="#search"]').on('click', function (event) {
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    $('#search, #search button.close').on('click keyup', function (event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
     // Headroom - show/hide navbar on scroll
-    if($('.headroom')[0]) {
-        var headroom  = new Headroom(document.querySelector("#navbar-main"), {
+    if ($('.headroom')[0]) {
+        var headroom = new Headroom(document.querySelector("#navbar-main"), {
             offset: 300,
-            tolerance : {
-                up : 30,
-                down : 30
+            tolerance: {
+                up: 30,
+                down: 30
             },
         });
         headroom.init();
     }
-    if($('.prettyprint').length) {
-			window.prettyPrint && prettyPrint();
-		}
+    if ($('.prettyprint').length) {
+        window.prettyPrint && prettyPrint();
+    }
     // Datepicker
-    $('.datepicker')[0] && $('.datepicker').each(function() {
+    $('.datepicker')[0] && $('.datepicker').each(function () {
         $('.datepicker').datepicker({
             disableTouchKeyboard: true,
             autoclose: false
@@ -45,25 +44,25 @@ $('a[href="#search"]').on('click', function(event) {
     $('[data-toggle="tooltip"]').tooltip();
 
     // Popover
-    $('[data-toggle="popover"]').each(function() {
+    $('[data-toggle="popover"]').each(function () {
         var popoverClass = '';
-        if($(this).data('color')) {
-            popoverClass = 'popover-'+$(this).data('color');
+        if ($(this).data('color')) {
+            popoverClass = 'popover-' + $(this).data('color');
         }
         $(this).popover({
             trigger: 'focus',
-            template: '<div class="popover '+ popoverClass +'" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+            template: '<div class="popover ' + popoverClass + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
         })
     });
-    
+
     // Additional .focus class on form-groups
-    $('.form-control').on('focus blur', function(e) {
+    $('.form-control').on('focus blur', function (e) {
         $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
-    }).trigger('blur');    
-    
+    }).trigger('blur');
+
     // NoUI Slider
     if ($(".input-slider-container")[0]) {
-        $('.input-slider-container').each(function() {
+        $('.input-slider-container').each(function () {
 
             var slider = $(this).find('.input-slider');
             var sliderId = slider.attr('id');
@@ -87,7 +86,7 @@ $('a[href="#search"]').on('click', function(event) {
                 }
             });
 
-            c.noUiSlider.on('update', function(a, b) {
+            c.noUiSlider.on('update', function (a, b) {
                 d.textContent = a[b];
             });
         })
@@ -106,7 +105,7 @@ $('a[href="#search"]').on('click', function(event) {
                 min: parseInt(c.getAttribute('data-range-value-min')),
                 max: parseInt(c.getAttribute('data-range-value-max'))
             }
-        }), c.noUiSlider.on("update", function(a, b) {
+        }), c.noUiSlider.on("update", function (a, b) {
             f[b].textContent = a[b]
         })
     }
@@ -116,10 +115,10 @@ $('a[href="#search"]').on('click', function(event) {
     $('[data-toggle="on-screen"]')[0] && $('[data-toggle="on-screen"]').onScreen({
         container: window,
         direction: 'vertical',
-        doIn: function() {
+        doIn: function () {
             //alert();
         },
-        doOut: function() {
+        doOut: function () {
             // Do something to the matched elements as they get off scren
         },
         tolerance: 200,
@@ -129,7 +128,7 @@ $('a[href="#search"]').on('click', function(event) {
     });
 
     // Scroll to anchor with scroll animation
-    $('[data-toggle="scroll"]').on('click', function(event) {
+    $('[data-toggle="scroll"]').on('click', function (event) {
         var hash = $(this).attr('href');
         var offset = $(this).data('offset') ? $(this).data('offset') : 0;
 
@@ -140,4 +139,65 @@ $('a[href="#search"]').on('click', function(event) {
 
         event.preventDefault();
     });
- });   
+
+    // 设置cookie
+    function setCookie(a,c){var b=30;var d=new Date();d.setTime(d.getTime()+b*24*60*60*1000);document.cookie=a+"="+escape(c)+";expires="+d.toGMTString()}
+    // 获取cookie
+    function getCookie(b){var a,c=new RegExp("(^| )"+b+"=([^;]*)(;|$)");if(a=document.cookie.match(c)){return unescape(a[2])}else{return null}}
+
+    // 核心函数
+    function inlojv_js_getqqinfo(){
+
+        // 获取cookie
+        if(getCookie('user_avatar') && getCookie('user_qq') ){
+
+            $('div.comment-user-avatar img').attr('src',getCookie('user_avatar'));
+            $('#qq').val(getCookie('user_qq'));
+        }
+
+        $('#qq').on('blur',function(){
+            
+            var qq=$('#qq').val(); // 获取访客填在qq表单上的qq数字，其中#qq表示QQ input标签上的id，改成你自己的！
+            
+            if (qq.length > 0) {
+                // ajax方法获取昵称
+                $.ajax({
+                    type: 'get',
+                    url:'https://www.wangquanwei.com/wp-content/themes/lolimeow/modules/fun-qqinfo.php?type=getqqnickname&qq='+qq,  // func_getqqinfo.php是后端处理文件，注意路径，127.0.0.1 改成你自己的域名
+                    dataType: 'jsonp',
+                    jsonp: 'callback',
+                    jsonpCallback: 'portraitCallBack',
+                    success: function(data) {
+                        // console.log(data);
+                        $('#author').val(data[qq][6]);	// 将返回的qq昵称填入到昵称input表单上，其中#author表示昵称input标签上的id，改成你自己的！
+                        $('#email').val($.trim(qq)+'@qq.com'); // 将获取到的qq，改成qq邮箱填入邮箱表单，其中#email表示邮箱input标签上的id，改成你自己的！
+                        // alert('已获取昵称！'); // 弹出警告
+                        setCookie('user_qq',qq);	// 设置cookie
+                    },
+                    error: function() {
+                        $('#qq,#author,#email').val(''); // 如果获取失败则清空表单，注意input标签上的id，改成你自己的！
+                        alert('糟糕，昵称获取失败！请重新填写。'); // 弹出警告
+                    }
+                });
+                // 获取头像
+                // $.ajax({
+                //     type: 'get',
+                //     url: 'http://localhost/wordpress/wp-content/themes/lolimeow/modules/fun-qqinfo.php?type=getqqavatar&qq='+qq, // func_getqqinfo.php是后端处理文件，注意路径，127.0.0.1 改成你自己的域名！
+                //     dataType: 'jsonp',
+                //     jsonp: 'callback',
+                //     jsonpCallback: 'qqavatarCallBack',
+                //     success: function(data) {
+                //         $('div.comment-user-avatar img').attr('src',data[qq]);	// 将返回的qq头像设置到你评论表单区域显示头像的节点上，div.comment-user-avatar img表示头像节点img标签，改成你自己的！
+                //         alert('已获取头像！'); // 弹出警告
+                //         setCookie('user_avatar',data[qq]);	 // 设置cookie
+                //     },
+                //     error: function() {
+                //         alert('糟糕，获取头像失败了！请重新填写。'); // 弹出警告
+                //         $('#qq,#yuao-comt-author,#yuao-comt-email').val(''); // 清空表单
+                //     }
+                // });
+            }
+
+        });
+    }
+});
--- functions.php
+++ functions.php
@@ -20,4 +20,27 @@ function md_version() {$versions = '1.6';   echo  $versions;}
 
 //以下可以自定义fun函数
 
+// 数据库插入评论表单的qq字段
+add_action('wp_insert_comment','inlojv_sql_insert_qq_field',10,2);
+function inlojv_sql_insert_qq_field($comment_ID,$commmentdata) {
+    $qq = isset($_POST['new_field_qq']) ? $_POST['new_field_qq'] : false;
+    update_comment_meta($comment_ID,'new_field_qq',$qq); // new_field_qq 是表单name值，也是存储在数据库里的字段名字
+}
+
+// 后台评论中显示qq字段
+add_filter( 'manage_edit-comments_columns', 'add_comments_columns' );
+function add_comments_columns( $columns ){
+    $columns[ 'new_field_qq' ] = __( 'QQ号' );        // 新增列名称
+    return $columns;
+}
+
+add_action( 'manage_comments_custom_column', 'output_comments_qq_columns', 10, 2 );
+function output_comments_qq_columns( $column_name, $comment_id ){
+    switch( $column_name ) {
+        case "new_field_qq" :
+            // 这是输出值，可以拿来在前端输出，这里已经在钩子manage_comments_custom_column上输出了
+            echo get_comment_meta( $comment_id, 'new_field_qq', true );
+            break;
+    }
+}
 
